0e5c45ef91ded6f56e500a215c8dad6ebfeb0239
xiaomingguohe <67264884+xiaomingguohe@users.noreply.github.com>
2020-12-21 20:56:51 +0800

    init

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc3791b98150dffa9300e276bd327077.js"
+  "/precache-manifest.ce8399db9d710646c293d524c59677bd.js"
 );
 
 self.addEventListener('message', (event) => {
